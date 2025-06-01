9ecd317eb6788d2b16d302859add8bdc4c7f5b46
JoaoVictorZenaide <joaovictorcarneiro2@gmail.com>
2025-06-01 20:29:32 -0300

    adicionado sistema de mini mapa

---
+++ Assets/Scripts/miniMap.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class miniMap : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
