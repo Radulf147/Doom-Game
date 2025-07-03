5e82c2905d021f65290bb41c8af26fb8e07655ff
Thiago <thiago_silva.9@hotmail.com>
2025-07-02 21:28:00 -0300

    Bug jogador fase 3

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -15,7 +15,7 @@ public class CharacterSelection : MonoBehaviour
 
         // Carrega a cena do jogo
 
-        SceneManager.LoadScene("fase1");
+        SceneManager.LoadScene("fase 3");
     }
 
     // Estas são as funções que os botões vão chamar
